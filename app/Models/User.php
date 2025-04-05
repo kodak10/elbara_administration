4a9c24e6cf782518c27b0da5feb89add0cb7bf1a
Kodak10 <atchinaymard10@gmail.com>
2025-04-05 19:09:09 +0200

    push

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -7,54 +6,71 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
-use Spatie\Permission\Traits\HasRoles;  
+use Spatie\Permission\Traits\HasRoles;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions; // Utilisation de LogOptions au lieu de ActivitylogOptions
 
 class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use  HasApiTokens, HasFactory, Notifiable, HasRoles;
+    use LogsActivity, HasApiTokens, HasFactory, Notifiable, HasRoles;
 
+    // Vous pouvez définir vos attributs à logger directement ici
+    protected static $logAttributes = ['name', 'email', 'updated_at'];
+
+    // Optionnel : cette méthode permet de personnaliser l'enregistrement des actions
+    protected static $logName = 'user';
+
+    // Personnalisation de la description des événements
+    public function getDescriptionForEvent(string $eventName): string
+{
+    switch ($eventName) {
+        case 'created':
+            return "{$this->name} a été créé";
+        case 'updated':
+            return "{$this->name} a été mis à jour";
+        case 'deleted':
+            return "{$this->name} a été supprimé";
+        case 'restored':
+            return "{$this->name} a été restauré";
+        case 'forceDeleted':
+            return "{$this->name} a été définitivement supprimé";
+        default:
+            return "{$this->name} a effectué une action sur l'utilisateur.";
+    }
+}
+
+    // Modification de la méthode pour utiliser LogOptions
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults() // Utilisez LogOptions::defaults() pour obtenir l'option par défaut
+            ->logOnly(['name', 'email']); // Par exemple, on log uniquement le nom et l'email
+    }
 
     public function createApiToken()
     {
         $token = Str::random(60);
-        
+
         $this->api_token = hash('sha256', $token);
         $this->save();
 
         return $token;
     }
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+
     protected $fillable = [
         'name',
         'email',
         'password',
-        'phone_number', 
-        'status', 
+        'phone_number',
+        'status',
     ];
 
-    
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -68,7 +84,6 @@ class User extends Authenticatable implements MustVerifyEmail
         return $this->hasMany(Order::class);
     }
 
-    // Relation avec les commandes (en tant que livreur)
     public function deliveredOrders()
     {
         return $this->hasMany(Order::class, 'livreur_id');
